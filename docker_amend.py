382e37df88704e9b2647181f3c854acb3bffafe4
Alexander Pushkov <alexander@notpushk.in>
2021-01-26 04:18:16 +0300

    Better volume parsing, don't remove container on bad status code

--- docker_amend.py
+++ docker_amend.py
@@ -1,9 +1,7 @@
 from functools import partial
-from io import BytesIO
-import json
 from os import getcwd
-from tarfile import TarFile
-from typing import List, Optional
+from os.path import abspath, normpath
+from typing import Iterable, List, Optional, Tuple
 
 import docker
 import typer
@@ -12,6 +10,25 @@ from typer.models import Required
 echo = partial(typer.echo, err=True)
 
 client = docker.from_env()
+
+
+def _parse_volumes(volumes: Iterable[str]) -> List[Tuple[str, dict]]:
+    def _parse_volume(volume: str) -> dict:
+        parts = volume.split(":")
+        if len(parts) > 3:
+            raise typer.BadParameter(
+                f"volume '{volume}' has incorrect format, should be SOURCE[:TARGET[:MODE]]",
+            )
+
+        source = abspath(parts[0])
+        target = normpath(parts[1]) if len(parts) > 1 else source
+
+        return source, {
+            "bind": target,
+            "mode": parts[2] if len(parts) > 2 else "rw",
+        }
+
+    return dict(_parse_volume(volume) for volume in volumes)
 
 
 def main(
@@ -28,21 +45,22 @@ def main(
         None,
         "-v",
         "--volume",
-        metavar="SOURCE:TARGET",
-        help="Bind mount a volume (assumes --no-pwd-volume)",
+        metavar="SOURCE[:TARGET[:MODE]]",
+        help="Bind mount a volume.",
+        callback=_parse_volumes,
     ),
     no_cwd_volume: bool = typer.Option(
-        False, "--no-cwd-volume", help="Do not mount cwd as a volume"
+        False,
+        "--no-cwd-volume",
+        help="Do not mount current working directory as a volume.",
     ),
 ):
     """
     Amend IMAGE by running COMMAND in a separate layer.
     """
 
-    volumes = {
-        source: {"bind": bind, "mode": "rw"}
-        for source, bind in (volume.split(":", 1) for volume in volumes)
-    }
+    if tag is None:
+        tag = image_name
 
     if not no_cwd_volume and getcwd() not in volumes:
         working_dir = client.images.get(image_name).attrs["Config"]["WorkingDir"]
@@ -55,19 +73,19 @@ def main(
         image_name, command, volumes=volumes, detach=True
     )
     container.start()
-    logs = container.attach(stdout=True, stderr=True, stream=True, logs=True)
-    for line in logs:
+    for line in container.attach(stdout=True, stderr=True, stream=True, logs=True):
+        # TODO: attach stdout to stdout
         echo(line, nl=False)
 
     status_code = container.wait()["StatusCode"]
     if status_code == 0:
-        container.commit(tag or image_name)
+        container.commit(tag)
         container.remove()
     else:
         echo(
-            f"Exited with code {status_code}, leaving {tag or image_name} at old version"
+            f"\nExited with code {status_code}, leaving {tag} at old version. If you still want to override it, run:\n"
+            + f"  docker commit {container.id} {tag}",
         )
-        container.remove()
 
 
 def _run():
